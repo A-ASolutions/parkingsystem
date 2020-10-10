a9fa5e04060e3bd2b09b4347c04a5fa5bc773721
aalshoaibi <69863696+aalshoaibi@users.noreply.github.com>
2020-10-10 19:12:14 +0100

    Corrected duration and adding 30 minutes free parking

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -7,14 +7,33 @@ public class FareCalculatorService {
 
     public void calculateFare(Ticket ticket){
         if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+           throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
+        /** We will use getTime method to get the time
+         as milliseconds and then convert it to hours.
+         The duration variable should be a double.
+         */
 
-        //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        long inHour = ticket.getInTime().getTime();
+        long outHour = ticket.getOutTime().getTime();
+
+
+
+        double duration = (double) (outHour -inHour) /(1000*3600);
+
+
+        /** create a condition if duration is less than
+         * thirty minutes, the duration will be 0.
+         * Else 30 minute will deducted from the duration
+         */
+        if (duration < 0.5)
+            duration = 0;
+        else
+            duration= duration - 0.5;
+
+
+
 
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
